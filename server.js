97e7a70b6ab9656a5e10bb0290acf48721edc105
Sheharyar Ahmed <sheryahmedme1@gmail.com>
2026-03-27 21:06:22 +0500

    feat: add Babel SSR entrypoint for React pizza menu

--- server.js
+++ server.js
@@ -1,16 +1,77 @@
 const { readFileSync } = require("fs");
 const { createServer } = require("http");
-const { parse } = require("url");
+const { renderToString } = require("react-dom/server");
+const React = require("react");
 
 const htmlTemplate = readFileSync(`${__dirname}/index.html`, "utf-8");
 
+const pizzas = [
+  {
+    name: "Focaccia",
+    price: 6,
+  },
+  {
+    name: "Pizza Margherita",
+    price: 10,
+  },
+  {
+    name: "Pizza Spinaci",
+    price: 12,
+  },
+  {
+    name: "Pizza Funghi",
+    price: 12,
+  },
+  {
+    name: "Pizza Prosciutto",
+    price: 15,
+  },
+];
+
+function Home() {
+  return (
+    <div>
+      <h1>🍕 Fast React Pizza Co.</h1>
+      <p>This page has been rendered with React on the server 🤯</p>
+
+      <h2>Menu</h2>
+      <ul>
+        {pizzas.map((pizza) => (
+          <MenuItem pizza={pizza} key={pizza.name} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Counter() {
+  const [count, setCount] = React.useState(0);
+  return (
+    <div>
+      <button onClick={() => setCount((c) => c + 1)}>+1</button>
+      <span>{count}</span>
+    </div>
+  );
+}
+
+function MenuItem({ pizza }) {
+  return (
+    <li>
+      <h4>
+        {pizza.name} (${pizza.price})
+      </h4>
+      <Counter />
+    </li>
+  );
+}
+
 const server = createServer((req, res) => {
-  const pathName = parse(req.url, true).pathname;
+  const pathName = new URL(req.url, "http://127.0.0.1:8000").pathname;
 
   if (pathName === "/") {
+    const renderedHtml = renderToString(<Home />);
     res.writeHead(200, { "Content-type": "text/html" });
-    res.end(htmlTemplate);
-    res.end("Hello world");
+    res.end(renderedHtml);
   } else if (pathName === "/test") {
     res.end("Test route working");
   } else {
