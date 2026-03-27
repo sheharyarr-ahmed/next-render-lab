7cea9a5abb6b4e513c9aca546776d851991bf9df
Sheharyar Ahmed <sheryahmedme1@gmail.com>
2026-03-27 20:45:30 +0500

    feat: add static HTML page and basic route handling

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 const { createServer } = require("http");
+const { parse } = require("url");
+
 const server = createServer((req, res) => {
-  res.end("Hello world");
+  const pathName = parse(req.url, true).pathname;
+
+  if (pathName === "/") {
+    res.end("Hello world");
+  } else if (pathName === "/test") {
+    res.end("Test route working");
+  } else {
+    res.end("THE URL CANNOT BE FOUND");
+  }
 });
+
 server.listen(8000, () => console.log("Listening for requests on port 8000"));
