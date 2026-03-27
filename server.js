7c5cbd7a954d9e978ee97c73b8fb843a1ea13ee2
Sheharyar Ahmed <sheryahmedme1@gmail.com>
2026-03-27 20:56:46 +0500

    build: add React SSR runtime and Babel server dependencies

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
+const { readFileSync } = require("fs");
 const { createServer } = require("http");
 const { parse } = require("url");
+
+const htmlTemplate = readFileSync(`${__dirname}/index.html`, "utf-8");
 
 const server = createServer((req, res) => {
   const pathName = parse(req.url, true).pathname;
 
   if (pathName === "/") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    res.end(htmlTemplate);
     res.end("Hello world");
   } else if (pathName === "/test") {
     res.end("Test route working");
